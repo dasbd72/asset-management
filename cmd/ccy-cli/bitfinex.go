58163faf3dd68c02d8d14e3dc119cdd01d33d699
dasbd72 <twbd723@gmail.com>
2024-03-02 12:52:43 +0800

    update cli bitfinex sub-command

--- cmd/ccy-cli/bitfinex.go
+++ cmd/ccy-cli/bitfinex.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/dasbd72/go-exchange-sdk/bitfinex"
@@ -18,26 +18,47 @@ func Bitfinex(cmd *cobra.Command, args []string) {
 		os.Getenv("BFX_API_SECRET"),
 	)
 
-	data, err := c.CallAPI(ctx, bitfinex.Request_builder{
-		Method:   http.MethodGet,
-		Endpoint: "/ticker",
-		SecType:  bitfinex.SecTypePrivate,
-		Params:   map[string]interface{}{},
-	}.Build())
-	if err != nil {
-		log.Fatal(err)
+	// data, err := c.CallAPI(ctx, bitfinex.Request_builder{
+	// 	Method:   http.MethodGet,
+	// 	Endpoint: "/ticker",
+	// 	SecType:  bitfinex.SecTypePrivate,
+	// 	Params:   map[string]interface{}{},
+	// }.Build())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(string(data))
+	{
+		data, err := c.GetWallets(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		b, err := json.MarshalIndent(data, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(string(b))
 	}
-	log.Println(string(data))
-	// data, err := c.GetWallets(ctx)
-	// data, err := c.GetActiveFundingOffers(ctx, "UST")
-	// data, err := c.GetFundingStats(ctx, "fUSD")
-	// data, err := c.GetWallets(ctx)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// b, err := json.MarshalIndent(data, "", "  ")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// log.Println(string(b))
+	{
+		data, err := c.GetActiveFundingOffers(ctx, "UST")
+		if err != nil {
+			log.Fatal(err)
+		}
+		b, err := json.MarshalIndent(data, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(string(b))
+	}
+	{
+		data, err := c.GetFundingStats(ctx, "UST")
+		if err != nil {
+			log.Fatal(err)
+		}
+		b, err := json.MarshalIndent(data, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(string(b))
+	}
 }
